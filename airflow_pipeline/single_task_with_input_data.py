d55328987156626fea72d12b42637d538f60ae01
Lam, Tuan Mau <63532380+lammau123@users.noreply.github.com>
2023-09-28 09:00:45 -0600

    Update single_task_with_input_data.py

--- airflow_pipeline/single_task_with_input_data.py
+++ airflow_pipeline/single_task_with_input_data.py
@@ -1,31 +1,36 @@
+# Import necessary modules from Airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+
+# Import modules for date and time handling
 from datetime import timedelta
 import pendulum
 
+# Define a Python function that represents the task's functionality
 def task_name_with_input(input_data):
     print('This is the demo of input data={data}'.format(data=input_data))
-    
+
+# Define default arguments for the DAG
 default_args = {
-    'owner': 'OwnerName',
-    #days_ago = 2 means current date - 2 days. This makes sure it schedule correctly to current day
-    'start_date': pendulum.today('UTC').add(days=-2),
-    #timedetla=1 daily schedule
-    'schedule_interval': timedelta(days=1)
+    'owner': 'OwnerName',  # Specify the owner or creator of the DAG
+    'start_date': pendulum.today('UTC').add(days=-2),  # Define the start date for task scheduling
+    'schedule_interval': timedelta(days=1)  # Define the schedule interval (in this case, daily)
 }
 
+# Create a new DAG instance with a unique ID, description, and tags
 with DAG(
-    dag_id = 'single_task_with_input',
-    description = 'This is a single task dag test.',
-    default_args = default_args,
-    tags = ['single_task_with_input']
+    dag_id='single_task_with_input',  # Unique identifier for the DAG
+    description='This is a single task DAG test.',  # Description of the DAG
+    default_args=default_args,  # Assign the default arguments defined earlier
+    tags=['single_task_with_input']  # Assign tags to categorize the DAG
 ) as dag:
+    # Define a PythonOperator task within the DAG
     task = PythonOperator(
-        task_id = 'single_task_with_input_id',
-        python_callable = task_name_with_input,
-        #input value for task
+        task_id='single_task_with_input_id',  # Unique identifier for the task
+        python_callable=task_name_with_input,  # Specify the Python function to execute for this task
+        # Define input data for the task using op_kwargs
         op_kwargs={'input_data': 'input value demo'}
     )
 
-##config pipeline here
+## Define task to be executed in pipeline
 task
