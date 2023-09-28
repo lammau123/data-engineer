719fa2f5882fd8d2e78a5679f49342ecc7941f15
Lam, Tuan Mau <63532380+lammau123@users.noreply.github.com>
2023-09-28 08:53:38 -0600

    Update single_task.py

--- airflow_pipeline/single_task.py
+++ airflow_pipeline/single_task.py
@@ -1,29 +1,33 @@
+# Import necessary modules from Airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+
+# Import modules for date and time handling
 from datetime import timedelta
 import pendulum
 
+# Define a Python function that represents the task's functionality
 def task_name():
-    print('This is single task demo.')
-    
+    print('This is a single task demo.')
+
+# Define default arguments for the DAG
 default_args = {
-    'owner': 'OwnerName',
-    #days_ago = 2 means current date - 2 days. This makes sure it schedule correctly to current day
-    'start_date': pendulum.today('UTC').add(days=-2),
-    #timedetla=1 daily schedule
-    'schedule_interval': timedelta(days=1)
+    'owner': 'OwnerName',  # Specify the owner or creator of the DAG
+    'start_date': pendulum.today('UTC').add(days=-2),  # Define the start date for task scheduling
+    'schedule_interval': timedelta(days=1)  # Define the schedule interval (in this case, daily)
 }
 
+# Create a new DAG instance with a unique ID and description
 with DAG(
-    dag_id = 'single_task',
-    description = 'This is a single task dag test.',
-    default_args = default_args,
-    tags = ['single_task']
+    dag_id='single_task',  # Unique identifier for the DAG
+    description='This is a single task DAG test.',  # Description of the DAG
+    default_args=default_args  # Assign the default arguments defined earlier
 ) as dag:
+    # Define a PythonOperator task within the DAG
     task = PythonOperator(
-        task_id = 'single_task_id',
-        python_callable = task_name
+        task_id='single_task_id',  # Unique identifier for the task
+        python_callable=task_name  # Specify the Python function to execute for this task
     )
 
-##config pipeline here
+# Configuration for the DAG is defined above, and the 'task' is the final step that represents the task itself.
 task
